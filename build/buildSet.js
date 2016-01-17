2a297d0444ef97841a540ad20d3c3cbb504fa358
Sergio Moura <sergio@moura.us>
2016-01-17 18:40:42 -0300

    First attempt on joining Token information on Set.

--- build/buildSet.js
+++ build/buildSet.js
@@ -31,16 +31,48 @@ setsToDo.serialForEach(function(arg, subcb)
 	}
 
 	tiptoe(
-		function build()
-		{
+		function build() {
 			rip.ripSet(targetSet.name, this);
 		},
-		function save(set)
-		{
+		function tokens(set) {
+			// Fixes tokens before saving.
+			var tokens = [];
+			var cards = [];
+
+			// Move tokens to their place.
+			set.cards.forEach(function (c) {
+				if (c.layout === 'token') {
+					tokens.push(c);
+				}
+				else {
+					cards.push(c);
+				}
+			});
+
+			set.cards = cards;
+			set.tokens = tokens;
+
+			// Add missing tokens.
+			var self = this;
+			fs.readFile(path.join(__dirname, '..', 'tokens', 'tokens.json'), 'utf8', function(err, data) {
+				if (err)
+					return(setImmediate(function() { self(err); }));
+				
+				var tokens = JSON.parse(data);
+				tokens.forEach(function(token) {
+					if (token.sets.indexOf(set.code) >= 0) {
+						// TODO: Check if token already exists
+						set.tokens.push(token);
+					}
+				});
+
+				self(null, set);
+			});
+		},
+		function save(set) {
 			shared.saveSet(set, this);
 		},
-		function finish(err)
-		{
+		function finish(err) {
 			subcb(err);
 		}
 	);
